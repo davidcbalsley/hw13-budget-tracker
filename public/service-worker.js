0883202fab0494c3e4ce1bc92c81e36621261d04
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-04-28 23:38:20 -0500

    set up static cache and data cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
-console.log("Guten Tag from your service-worker.js file!");
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// Install and register service worker
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
